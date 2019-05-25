d4cc7e7a1ad50ccc2bdfc52200156a977cb72589
Mohammed <38591075+M507@users.noreply.github.com>
2019-05-25 02:40:17 -0400

    Create inject.js

---
+++ Extension/inject.js
@@ -0,0 +1,8 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+    if (changeInfo.status === 'complete') {
+        chrome.tabs.executeScript(tabId, {
+            allFrames: true, 
+            file: 'log.js'
+        });
+    }
+});
